fe66bcecb94517814d8e1baa98c175d1acf7c2c8
Haroon2k <bash1_15@uni.worc.ac.uk>
2016-10-18 11:26:27 +0100

    1.6
    dgd

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@ window.localStorage.setItem( key2, value2 );
 window.localStorage.setItem( key3, value3 );        
 window.localStorage.setItem( key4, value4 ); 
 window.localStorage.setItem( key5, value5 );
-alert(window.localStorage.getItem(key1);
+alert(window.localStorage.getItem(key1));
     }
 
 	
